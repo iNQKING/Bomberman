cd6b92ff473c3de32cd0f40f079888f620bf365f
Hutchii <ho7ch1@gmail.com>
2017-04-03 18:51:42 +0200

    Dodalem klase Plansza oraz pare metod.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,41 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-public class Program
+namespace Bomberman
 {
-	public Program()
-	{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Plansza plansza1 = new Plansza(20, 20);
+            plansza1.addSpawnPoint(10, 6);
+            ;
+        }
+        class Plansza
+        {
+            private int[,] rozmiarPlanszy;
 
-        test;
-	}
+            public Plansza(int x, int y)
+            {
+
+                rozmiarPlanszy = new int[x, y];
+            }
+
+            public void addSpawnPoint(int x, int y)
+            {
+                rozmiarPlanszy[x, y] = 2;
+            }
+            public void addObstacle(int x, int y)
+            {
+                List<int> list = new List<int>();
+                list.Add(2);
+                list.Add(3);
+                list.Add(5);
+                list.Add(7);
+            }
+        }
+    }
 }
