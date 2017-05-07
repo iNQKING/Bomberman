4a0dc1e365e96a44b131264cf3693039b60c9770
Bazyliarz <enigmus94@gmail.com>
2017-05-07 13:02:19 +0200

    Add files via upload

---
+++ Bomberman/Bomberman/ConsoleApplication.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    class ConsoleApplication
+    {
+        ConsoleOperation console = new ConsoleOperation();
+        Map map = new Map();
+        Snake snake = new Snake();
+        InteractionUser interaction = new InteractionUser();
+
+        public void Run()
+        {
+            int choice;
+            String choiceString;
+
+            try
+            {
+                do
+                {
+                    Console.Write("\n\n\t\tWITAJ W GRZE BOMBERMAN! \n\n");
+                    Console.Write("\t\t1. Graj\n");
+                    Console.Write("\t\t2. Jak grać?\n");
+                    Console.Write("\t\t3. O grze\n");
+                    Console.Write("\t\t0. Wyjście z programu\n");
+                    Console.Write("\n\n\t\tTwój wybór: ");
+
+                    choiceString = Console.ReadLine();
+                    choice = int.Parse(choiceString);
+
+                    switch (choice)
+                    {
+                        case 1:
+                            Console.Clear();
+                            while (true)
+                            {
+                                console.ClearMap(map);
+                                console.DrawMap(map);
+                                console.DrawSnake(snake, map);
+                                interaction.Move(snake);
+                                if (check.CrashWall(snake) || check.CrashBody(snake, map))
+                                {
+                                    Console.WriteLine("\n\nKONIEC!   LICZBA ZDOBYTYCH PUNKTÓW: {0}", console.Score);
+                                    Console.Beep(1000, 1000);
+                                    break;
+                                }
+                            }
+                            break;
+
+                        case 0:
+                            Console.Write("\n\n\n\n\t\tŻYCZĘ MIŁEGO DNIA :)");
+                            break;
+                    }
+                } while (choice == 1 || choice == 2 || choice == 3);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(String.Format("\nWystąpił błąd w programie: {0}", e.Message));
+            }
+        }
+    }
+}
