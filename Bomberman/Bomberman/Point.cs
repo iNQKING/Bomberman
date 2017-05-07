31cbc9ba30bd8fb9a787fee42902e8d9c4c002e8
Łukasz Grześkowiak <lukaszenko7@o2.pl>
2017-05-07 13:17:45 +0200

    Point (aktulna pozycja)

---
+++ Bomberman/Bomberman/Point.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bomberman
+{
+    class Point
+    {
+        private int x;
+        private int y;
+
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+
+        public int X
+        {
+            get { return this.x; }
+            set { x = value; }
+        }
+
+        public int Y
+        {
+            get { return this.y; }
+            set { y = value; }
+        }
+    }
+}
