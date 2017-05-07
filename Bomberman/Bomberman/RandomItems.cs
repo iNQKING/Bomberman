feb7eaeae91b5a27ad8cc6e2d63153d4ba22c579
Hutchii <ho7ch1@gmail.com>
2017-05-07 13:12:41 +0200

    powerUp test.

---
+++ Bomberman/Bomberman/RandomItems.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bomberman
+{
+    class RandomItems
+    {
+        static Random rand = new Random();
+        Point powerUp;
+
+
+        public Point RandomApple(Map map)
+        {
+            do
+            {
+                powerUp = new Point(rand.Next(0, 19), rand.Next(0, 69));
+
+            } while (map.ArrayMap[powerUp.X, powerUp.Y] != ' ');
+
+            return powerUp;
+        }
+
+
+        //public Point powerUp
+        //{
+        //    get { return powerUp; }
+        //}
+    }
+}
