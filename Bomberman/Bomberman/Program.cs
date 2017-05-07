8b6560945fabff4475b795c32ad755871049aa5f
Łukasz Grześkowiak <lukaszenko7@o2.pl>
2017-05-07 13:42:52 +0200

    Main update

--- Bomberman/Bomberman/Program.cs
+++ Bomberman/Bomberman/Program.cs
@@ -11,39 +11,9 @@ namespace Bomberman
     {
         static void Main(string[] args)
         {
-            Plansza plansza1 = new Plansza(20, 20);
-            plansza1.addSpawnPoint(10, 6);
-            ;
-        }
-
-        enum TypObiektu
-        {
-            None = 1,
-            SpawnPoint = 2
-        }
-
-        class Plansza
-        {
-            private TypObiektu[,] rozmiarPlanszy;
-
-            public Plansza(int x, int y)
-            {
-
-                rozmiarPlanszy = new TypObiektu[x, y];
-            }
-
-            public void addSpawnPoint(int x, int y)
-            {
-                rozmiarPlanszy[x, y] = TypObiektu.SpawnPoint;
-            }
-            public void addObstacle(List<Point> obstacle)
-            {
-                List<int> list = new List<int>();
-                list.Add(2);
-                list.Add(3);
-                list.Add(5);
-                list.Add(7);
-            }
+            ConsoleApplication aplication = new ConsoleApplication();
+            aplication.Run();
+            Console.ReadKey();
         }
     }
 }
